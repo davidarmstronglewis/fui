fbc9b9f5199b79c57f7e925973be186156108da0
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2018-03-21 11:30:48 +0100

    Fix Fui data by adding settable attrs: name, version, description, author

--- examples/app_basic.rs
+++ examples/app_basic.rs
@@ -1,5 +1,6 @@
 // Example showing imagined CLI app. with two actions
 
+#[macro_use]
 extern crate clap;
 extern crate fui;
 
@@ -27,6 +28,14 @@ fn main() {
             FormView::new().field(Text::new("action2-data").help("help for action2 data")),
             hdlr,
         )
-        .about("An Example program which has CLI & form interface (TUI)")
+        // optional attrs, however it's recommended to set them otherwise "" will be used
+        // here values are taken from Cargo.toml (see: clap doc https://docs.rs/clap/)
+        // NOTE:
+        // Running this example will show program name as "fui" (instead of "app_basic")
+        // because that's the value in Cargo.toml (similar to rest of attrs)
+        .name(crate_name!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .author(crate_authors!())
         .run();
 }
